2d6f9b52d85130775cf2e9617e0c22bd2d466fa1
Armando González <A01721627@itesm.mx>
2021-08-27 14:47:41 -0500

    Tarea 26Agosto

--- assignments/03TipoTriangulo/src/exercise.py
+++ assignments/03TipoTriangulo/src/exercise.py
@@ -6,8 +6,10 @@ def main():
         print("ES UN TRIANGULO EQUILATERO")
     elif (lado1== lado2 or lado2== lado3 or lado3==lado1) and (lado1!=lado3 or lado3!=lado2 or lado1!=lado2):
         print("ES UN ISOSCELES")
-    elif (lado1!=lado2 and lado2!=lado3 and lado3!=lado1):
+    elif (lado1!=lado2 and lado2!=lado3 and lado3!=lado1) and ((lado1**2==lado2**2+lado3**2)or(lado2==lado3**2+lado1**2)or(lado3==lado2**2+lado1**2)):
         print("ES UN TRIANGULO ESCALENO")
+    elif (lado1>(lado2+lado3))or(lado3>(lado1+lado2))or(lado2>(lado1+lado3)):
+        print('NO ES UN TRIANGULO')
 
 
     
