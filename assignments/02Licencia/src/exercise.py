b9d9369f4b95c9d1f71898b7b954c999988fc1e4
Armando González <A01721627@itesm.mx>
2021-08-27 09:54:58 -0500

    TareaAgosto26

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,13 @@
 
 def main():
     edad = int(input("Ingresa tu edad: "))
+    id= str(input("Tienes identificación oficial? (s/n)"))
+    if edad>=18 and id=="s":
+        print("Tramite de licencia concedido")
+    elif edad<18:
+        print("No cumples los requisitos")
+    elif id!="s" or "n":
+        print("Respuesta incorrecta")
     #Aquí empieza tu programa...
 
 if __name__ == '__main__':
--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,6 +1,22 @@
 def main():
     #escribe tu código abajo de esta línea
-    
+    peso = float(input("Peso en kg: "))
+    altura= float(input("Altura en m: "))
+
+    bmi=peso/altura**2
+
+    if bmi<20 and bmi>0:
+        print ("PESO BAJO")
+    elif bmi>=20 and bmi<25:
+        print("NORMAL")
+    elif bmi>=25 and bmi<30:
+        print("SOBREPESO")
+    elif bmi>=30 and bmi<40:
+        print("OBESIDAD")
+    elif bmi <=40:
+        print("OBESIDAD MORIBUNDA")
+    elif bmi<=0:
+        print("Revisa tus datos, alguno de ellos es erróneo")
 
 if __name__=='__main__':
     main()
